061e878237885212f3abdcd92c8f63a4ea250ae4
James Bain <jamescbain@gmail.com>
2016-03-15 10:35:16 -0500

    create new columns with final categories

--- body_part_joining.R
+++ body_part_joining.R
@@ -7,5 +7,14 @@ df <- read.csv('Data/NEISS/sport_category_final.csv')
 orig_table <- table(df$prod1,df$body_part)
 
 # create a datafame from the contigency table (at least one that is intuitive)
-tableMatrix<-as.data.frame.matrix(orig_table)
+tm<-as.data.frame.matrix(orig_table) # tm stands for table matrix
+
+# create new columns based on body part combinations
+head_neck <- tm$face + tm$neck + tm$head
+shoulder_arm <- tm$`upper arm` + tm$`lower arm` + tm$shoulder + tm$elbow
+hand <- tm$hand + tm$finger + tm$wrist
+pelvis <- tm$`pubic region` + tm$hip
+leg <- tm$knee + tm$`lower leg` + tm$`upper leg`
+foot <- tm$foot + tm$toe + tm$ankle
+trunk <- tm$`upper trunk` + tm$back
 
