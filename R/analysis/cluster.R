dfeed6d045787ec2b64430e373fadaf2c05d6dbc
James Bain <jamescbain@gmail.com>
2017-06-01 00:15:36 -0500

    save an agnes dendrogram

--- R/analysis/cluster.R
+++ R/analysis/cluster.R
@@ -9,6 +9,8 @@ library(tidyr)
 library(dplyr)
 library(devtools)
 library(ggbiplot)
+library(cluster)
+library(ggdendro)
 
 file <- '~/Documents/research/neander_trauma/Data/contingency_norm.csv'
 file2<- '~/Documents/research/neander_trauma/Data/neander_contingency_norm.csv'
@@ -23,17 +25,30 @@ berger <- data.frame(X = c('bt5','libben','nubia','london','new york','new mexic
                      hand = c(14,0,3,421,2624,187,11),
                      pelvis = c(7,0,6,3,65,17,6),
                      leg = c(20,37,36,409,2466,88,11),
-                     foot = c(14,3,2,121,670,205,17)
+                     foot = c(14,3,2,121,670,205,17),
+                     total = c(223,94,160,1730,11959,792,181)
                      )
 
+berg_norm <-berger %>% 
+  mutate(head_neck = head_neck/total,
+         trunk = trunk/total,
+         shoulder_arm = shoulder_arm/total,
+         hand = hand/total,
+         pelvis = pelvis/total,
+         leg = leg/total,
+         foot = foot/total) %>%
+  select(X,head_neck,shoulder_arm,hand,pelvis,leg,foot,trunk)
 
 df <- rbind(activities,neander)
 
+df <- df %>%
+  select(-c(`X.1`))
+
+df <- rbind(df, berg_norm)
 rownames(df) <- df$X
 
 
-df <- df %>%
-  select(-c(`X.1`))
+
 
 k <- kmeans(df[2:8],6, nstart = 25)
 
@@ -65,3 +80,27 @@ g <- g + theme(legend.direction = 'horizontal',
 
 clusters <- hclust(dist(df[,2:8]))
 plot(clusters)
+
+
+# Exploring
+
+d <- dist(df[2:8])
+act.hclust <- hclust(d)
+
+group.25<-cutree(act.hclust,25)
+pam.act <- pam(d,10)
+k <- kmeans(df[2:8],10)
+
+table(group.25, pam.act$clustering)
+table(group.25,k$cluster)
+
+plot(act.hclust,main='Default from hclust')
+
+model <- agnes(d)
+dg <- as.dendrogram(model)
+ggdendrogram(dg)
+
+
+cutree(model,25)
+t <- table(cutree(model,25),group.25)
+sum(diag(t))/sum(t)
